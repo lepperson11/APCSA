a8030a5f4612c05d243481c6f621fc61737068c7
lepperson11 <EppersonL1493@sduhsd.lan>
2018-02-21 09:30:04 -0800

    hk;lh

--- Unit7/Unit7-2016/Unit7-Assignments/GuessingGame.java
+++ Unit7/Unit7-2016/Unit7-Assignments/GuessingGame.java
@@ -6,22 +6,27 @@
 
 import java.util.Scanner;
 import static java.lang.System.*;
+import java.util.Random;
+
 
 public class GuessingGame
 {
-	private int upperBound;
-
-	public GuessingGame(int stop)
+	private int upperbound, guess;
+	Scanner keyboard = new Scanner(System.in);
+	
+	public void Guessing(int stop)
 	{
-
+		upperbound = stop;
 
 	}
 
 	public void playGame()
 	{
-		Scanner keyboard = new Scanner(System.in);
+		Random rand = new Random();
 
-
+		int n = rand.nextInt(upperbound) + 1;
+		System.out.println("Pick a number between 1 and " + upperbound);
+		int guess = keyboard.nextInt();
 
 
 
